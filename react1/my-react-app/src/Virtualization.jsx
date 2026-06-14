b0868de2a0e803a9de07ea52f7766797da842a07
Sangram <sangram2mail@gmail.com>
2026-06-14 10:03:48 +0530

    Virtual List

---
+++ react1/my-react-app/src/Virtualization.jsx
@@ -0,0 +1,20 @@
+import {FixedSizeList as List} from 'react-window';
+
+//Define Row Renderer
+const Row = ({index, style}) => (
+    <div className={index % 2 === 0 ? 'row-even': 'row-odd'}>
+        Row Item { index + 1}
+    </div>
+);
+
+
+const VirtualList = () => {
+    return (
+        <List height={500} width={400} itemCount={50000} itemSize={50}>
+            {Row}
+        </List>
+    )
+}
+
+export default VirtualList;
+
